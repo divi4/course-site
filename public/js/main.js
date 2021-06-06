a97589fd439616eddc3ad1829298801c75aef9f5
Divi <73816974+divi4@users.noreply.github.com>
2021-06-07 07:05:05 +1000

    Added DOM JS

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".wkBtns").forEach((el) => {
+  el.addEventListener("click", function(e){
+    console.log(e)
+    updateWkVids(e))
+  }
+});
+
+function updateWkVids(e) {
+  console.log(e)
+}

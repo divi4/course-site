f56ac34f37576df4f1d9bc2ff087f6848ecfcd82
Divi <73816974+divi4@users.noreply.github.com>
2021-08-04 18:20:16 +1000

    Identified bug: wkBtns class is not being detected by DOM javascript

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
-document.querySelectorAll(".wkBtns").forEach((el) => {
-  el.addEventListener("click", function(e){
-    console.log(e)
-    updateWkVids(e))
-  }
-});
+console.log("hoisrnoiesrtan")
+
+// let x = document.getElementsByClassName("wkBtns")
+
+// wkBtns class is not being found within the html
+
+// x.item(0).addEventListener("click", function(e){
+//     console.log(e)
+//     updateWkVids(e)
+// }, false);
 
 function updateWkVids(e) {
   console.log(e)
